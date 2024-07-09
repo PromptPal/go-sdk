c55ba13ee14675f185f371a26f62fbefbd756886
AnnatarHe <annatar.he+github@gmail.com>
2024-07-10 02:10:49 +0800

    feat(stream): add streaming api support

--- promptpal/client.go
+++ promptpal/client.go
@@ -1,7 +1,9 @@
 package promptpal
 
 import (
+	"bufio"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"time"
@@ -17,6 +19,7 @@ type promptPalClient struct {
 
 type PromptPalClient interface {
 	Execute(ctx context.Context, prompt string, variables any, userId *string) (*APIRunPromptResponse, error)
+	ExecuteStream(ctx context.Context, prompt string, variables any, userId *string, onData func(data *APIRunPromptResponse) error) (*APIRunPromptResponse, error)
 }
 
 type PromptPalClientOptions struct {
@@ -73,4 +76,56 @@ func (p *promptPalClient) Execute(ctx context.Context, prompt string, variables 
 		return nil, errors.New("invalid prompt response type")
 	}
 	return result, nil
+}
+
+func (p *promptPalClient) ExecuteStream(ctx context.Context, prompt string, variables any, userId *string, onData func(data *APIRunPromptResponse) error) (*APIRunPromptResponse, error) {
+	payload := apiRunPromptPayload{
+		Variables: variables,
+	}
+	if userId != nil {
+		payload.UserId = *userId
+	}
+
+	resp, err := p.client.R().
+		SetBody(payload).
+		SetPathParam("pid", prompt).
+		SetResult(APIRunPromptResponse{}).
+		SetError(errorResponse{}).
+		SetContext(ctx).
+		Post("/api/v1/public/prompts/run/{pid}/stream")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.RawResponse.Body.Close()
+
+	scanner := bufio.NewScanner(resp.RawResponse.Body)
+	var lastChunk *APIRunPromptResponse
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+		_res := scanner.Bytes()
+		if len(_res) == 0 {
+			continue
+		}
+
+		var chunkData APIRunPromptResponse
+
+		err = json.Unmarshal(_res, &chunkData)
+		if err != nil {
+			return nil, err
+		}
+
+		onData(&chunkData)
+		lastChunk = &chunkData
+	}
+
+	// if res.IsError() {
+	// 	errMsg := res.Error().(*errorResponse)
+	// 	return nil, fmt.Errorf("error: %d %s", errMsg.ErrorCode, errMsg.ErrorMessage)
+	// }
+
+	return lastChunk, nil
 }
